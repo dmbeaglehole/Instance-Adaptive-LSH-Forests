97b8ec4afbcc286b073226a125c84000c0431383
dmbeaglehole <84294223+dmbeaglehole@users.noreply.github.com>
2021-06-03 00:47:11 -0400

    Update local_gen_trees.py

--- local_gen_trees.py
+++ local_gen_trees.py
@@ -3,7 +3,7 @@ import os
 num_trees = 1
 for t in range(num_trees):
     tree_label = "tree" + str(t)
-    dir_path = "/home/dmb2266/mnist_v1/trees/" + tree_label + "/"
+    dir_path = "../mnist_v1/trees/" + tree_label + "/"
     os.system("mkdir " + dir_path)
     os.system("mkdir " + dir_path + "dists/")
     os.system("mkdir " + dir_path + "hard_queries/")
